341fb978d9ae632fb692a9ff84f6cb5cf5f94b9d
dimitri321 <dimitri321@163.com>
2016-12-05 15:36:31 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# zuowei
-call me zuowei~~
+nice~~
---
+++ cli.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+const path = require('path')
+const cac = require('cac')
+const termImg = require('term-img')
+const chalk = require('chalk')
+const Conf = require('conf')
+
+const config = new Conf()
+
+const cli = cac()
+
+const names = [
+  {
+    name: '作为',
+    romaji: 'Tachibana taki',
+    image: path.join(__dirname, 'images/taki.png')
+  },
+  {
+    name: 'xx~~',
+    romaji: 'Miyamizu Mitsuha',
+    image: path.join(__dirname, 'images/mitsuha.png')
+  }
+]
+
+cli.command('*', 'Tell me your name;)', () => {
+  // const prev = config.get('prev') || 0
+  // config.set('prev', prev === 0 ? 1 : 0)
+  const your = names[0]
+
+  console.log(`${your.name} ${chalk.dim(`[${your.romaji}]`)}`)
+  termImg(your.image, {height: '60%', fallback() {}})
+})
+
+cli.parse()
